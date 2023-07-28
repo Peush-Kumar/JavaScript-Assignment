286c7073fb2d3c95da52d91d70ecf9cba0e64076
Peush-Kumar <peushkumar28@gmail.com>
2023-07-28 03:52:10 -0700

    Updated at evening on friday

---
+++ script.js
@@ -0,0 +1,23 @@
+var name = "name:-"
+var fathername = "Father name:-"
+var email = "Email:-"
+var password = "Password:-"
+var CNIC = "CNIC Number:-"
+var mobile = "Mobile Number:-"
+var Courses = "Select Courses:-"
+var Qualification = "Select Qualification:-"
+var Gender = "Select Gender:-"
+var Photo = "Select Photo:-"
+var space = " "
+
+
+console.log( name+ space +prompt(name))
+console.log( fathername+ space +prompt(fathername))
+console.log( email+ space +prompt(email))
+console.log( password+ space +prompt(password))
+console.log( CNIC+ space +prompt(CNIC))
+console.log( mobile+ space +prompt(mobile))
+console.log( Courses+ space +prompt(Courses))
+console.log( Qualification+ space +prompt(Qualification))
+console.log( Gender+ space +prompt(Gender))
+console.log(Photo+ space +prompt(Photo))
